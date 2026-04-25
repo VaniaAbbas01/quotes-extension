96abbcbdb936bc4402f7d5b0d1651f7fdaee5928
unknown <vaniaabbas2@gmail.com>
2026-04-25 19:43:50 +0500

    added assets and quotes data

---
+++ data/quotes.js
@@ -0,0 +1,14 @@
+[
+  {
+    text: "Discipline is choosing between what you want now and what you want most.",
+    author: "Unknown",
+  },
+  {
+    text: "Small progress is still progress.",
+    author: "Unknown",
+  },
+  {
+    text: "Success is built on consistency, not motivation.",
+    author: "Unknown",
+  },
+];
